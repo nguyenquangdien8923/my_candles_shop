19e7487c454892cbb1cb95212cdbb130135024c8
Nguyen Quang Dien <quangdien0809@gmail.com>
2025-11-07 03:44:03 +0700

    Update route v2

--- src/Header.jsx
+++ src/Header.jsx
@@ -23,9 +23,9 @@ export default function Header() {
           <nav
             className={`hidden md:flex items-center space-x-8 text-lg font-semibold ${primaryColor}`}
           >
-            <a href="/" className={`${hoverColor} transition`}>Trang chủ</a>
-            <a href="/products" className={`${hoverColor} transition`}>Sản phẩm</a>
-            <a href="/about" className={`${hoverColor} transition`}>Về chúng tôi</a>
+            <a href="/my_candles_app/#/" className={`${hoverColor} transition`}>Trang chủ</a>
+            <a href="/my_candles_app/#/products" className={`${hoverColor} transition`}>Sản phẩm</a>
+            <a href="/my_candles_app/#/about" className={`${hoverColor} transition`}>Về chúng tôi</a>
           </nav>
         </div>
 
